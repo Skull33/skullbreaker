b0f62ea19f24765854050e52a4a6c8ab4bc12360
Skull33 <145301098+Skull33@users.noreply.github.com>
2026-05-29 00:19:00 -0500

    clases graficas y kernel añadidas

--- Skullbreaker.cpp
+++ Skullbreaker.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <string>
+#include "Skull_breaker_graphics.h"
+#include "skull_breaker_kernel.h"
 
 using namespace std;
 
 int main() {
-    string command[] = {"salir"};
+    cout<<"Este el el primer mensaje del SkullBreaker"<<endl;
+    string command[] = {"salir","editar"};
     while (true) {
         string comando;
-        cout<<"Este el el primer mensaje del SkullBreaker"<<endl;
         cout<<"------------------------------------------"<<endl;
         cout<<"ingrese un comando:"<<endl;
         cin>>comando;
         if (comando == "salir") {
             return 0;
+        }
+        else if (comando == "editar") {
+            cout<<"Bienvenido a SkullBreaker"<<endl;
+        }
+        else {
+            cout<<"------------------------------------------------------------"<<endl;
+            cout<<"el comando "<<comando<<" no existe o no ha sido implementado"<<endl;
+            cout<<"------------------------------------------------------------"<<endl;
         }
     }
 }
